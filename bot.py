5dd829c6dc5cf4ed4421e7fe9037c2066b43b9c6
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-06-27 07:27:19 +0600

    class BotText

--- bot.py
+++ bot.py
@@ -1,25 +1,44 @@
 import os
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentTypes
 from dotenv import load_dotenv
-from main import *
+from main import BotText
 
 load_dotenv()
 
-telegram_key = os.getenv('API_KEY')
+telegram_key = os.getenv('API_KEY') 
 bot = Bot(token=telegram_key)
 dp = Dispatcher(bot)
 
-kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kb.add(KeyboardButton('/help - Инструкция'))
-
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer('Приветствие', reply_markup=kb)
+    kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    kb.add(KeyboardButton('/help - Instruction'))
+    await message.answer(text=BotText().text_welcome(), reply_markup=kb)
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
-    await message.answer("/start - Приветствие\n/help - Инструкция\n/setting - Настройка")
+    await message.answer("/start - Welcome\n/help - Instruction\n/language - language")
+
+@dp.message_handler(commands=['language'])
+async def send_language(message: types.Message):
+    pass
+
+@dp.message_handler(content_types=['voice'])
+async def send_audio(message: types.Message):
+    pass
+
+@dp.message_handler(content_types=['photo'])
+async def send_photo(message: types.Message):
+    pass
+
+@dp.message_handler(content_types=['text'])
+async def send_text(message: types.Message):
+    pass
+
+@dp.message_handler(content_types=ContentTypes.ANY)
+async def send_all(message: types.Message):
+    await message.answer("The bot does not process such a message!")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates = True)
+    executor.start_polling(dp, skip_updates=True)
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-class Requests:
+
+class BotText:
+    def __init__(self) -> None:
+        pass
+    def text_welcome(self):
+        with open("~/text_bot/text_welcome.tex", "r") as f:
+            self.start_text = f.read()
+        return self.start_text
+
+class Connect:
     def __init__(self, url: str):
         self.url = url
         
@@ -10,4 +19,8 @@ class Requests:
             return self.r.content
         except requests.ConnectionError as e:
             print(e)
+
+# bot = BotText()
+# print(bot.text_welcome())
+
 # Здесь будет парсер
