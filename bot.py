71fd811688d08a015a7d6b76f276fd0347192daa
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-06-28 20:53:25 +0600

    commands language

--- bot.py
+++ bot.py
@@ -1,43 +1,54 @@
 import os
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentTypes
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ContentTypes, InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 from main import Botrecord
 
 load_dotenv()
 
 telegram_key = os.getenv('API_KEY') 
+admin = os.getenv('ADMIN')
 bot = Bot(token=telegram_key)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    kb.add(KeyboardButton('/help - Instruction'))
+    kb.add(KeyboardButton('/help - 🆘Instruction🆘'))
     await message.answer(text=Botrecord().text_welcome(), reply_markup=kb)
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
-    await message.answer("/start - Welcome\n/help - Instruction\n/language - language")
+    await message.answer("🤖/start - Welcome🤖\n🆘/help - Instruction🆘\n🌐/language - language🌐")
 
 @dp.message_handler(commands=['language'])
-async def send_language(message: types.Message):
-    pass
+async def weather_in(message: types.Message):
+    kb = InlineKeyboardMarkup(row_width=3)
+    btn1 = InlineKeyboardButton(text='English🇺🇸', callback_data='btn1')
+    btn2 = InlineKeyboardButton(text='Русский🇷🇺', callback_data='btn2')
+    btn3 = InlineKeyboardButton(text='Кыргызча🇰🇬', callback_data='btn3')
+    kb.add(btn1, btn2, btn3)
+    await message.answer('🌐Choose language:🌐', reply_markup=kb)
+
+@dp.callback_query_handler()
+async def check_callback_data(callback: types.CallbackQuery):
+    await callback.answer(text="aaa")
+    print(callback.data)
 
 @dp.message_handler(commands=['bot'])
 async def send_bot(message: types.Message):
-    pass
+    if message.chat.id == admin:
+        await message.answer(text=Botrecord())
 
-@dp.message_handler(content_types=['text','voice','audio'])
+@dp.message_handler(content_types=['text'])
 async def send_text(message: types.Message):
-    if message.content_type == "text":
-        pass
-    elif message.content_type == "voice" and message.content_type == "audio":
-        pass
+    sent_message = await message.answer("⌛️The request will take some time to process.⌛️")
+    await message.answer()
+    await bot.delete_message(message.chat.id, sent_message.message_id)
 
 @dp.message_handler(content_types=ContentTypes.ANY)
 async def send_all(message: types.Message):
-    await message.answer("The bot does not process such a message!")
+    await message.answer("💤The bot does not process such a message!💤")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
 import requests
+import psycopg2
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Connect:
     def __init__(self, url: str):
@@ -14,75 +19,75 @@ class Connect:
 
 class Botrecord:
     def text_welcome(self):
-        with open("/Users/ajbekisaev/Desktop/dungeon_master/telegram_bot/bot_gpt_hackathon/text_bot/text_welcome.tex", "r") as f:
+        with open("~/bot_hackathon/text_bot/text_welcome.tex", "r") as f:
             self.start_text = f.read()
         return self.start_text
-    # try:
-    #     # connect to exist database
-    #     connection = psycopg2.connect(
-    #         host=host,
-    #         user=user,
-    #         password=password,
-    #         database=db_name    
-    #     )
-    #     connection.autocommit = True
+    def db_language(self):
+        try:
+            # connect to exist database
+            connection = psycopg2.connect(
+                host=os.getenv('host'),
+                user=os.getenv('user'),
+                password=os.getenv('password'),
+                database=os.getenv('db_name')   
+            )
+            connection.autocommit = True
+            
+            # the cursor for perfoming database operations
+            # cursor = connection.cursor()
+            
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT version();"
+                )
+                
+                print(f"Server version: {cursor.fetchone()}")
+                
+            # create a new table
+            # with connection.cursor() as cursor:
+            #     cursor.execute(
+            #         """CREATE TABLE users(
+            #             id serial PRIMARY KEY,
+            #             first_name varchar(50) NOT NULL,
+            #             nick_name varchar(50) NOT NULL);"""
+            #     )
+                
+            #     # connection.commit()
+            #     print("[INFO] Table created successfully")
+                
+            # insert data into a table
+            # with connection.cursor() as cursor:
+            #     cursor.execute(
+            #         """INSERT INTO users (first_name, nick_name) VALUES
+            #         ('Oleg', 'barracuda');"""
+            #     )
+                
+            #     print("[INFO] Data was succefully inserted")
+                
+            # get data from a table
+            # with connection.cursor() as cursor:
+            #     cursor.execute(
+            #         """SELECT nick_name FROM users WHERE first_name = 'Oleg';"""
+            #     )
+                
+            #     print(cursor.fetchone())
+                
+            # delete a table
+            # with connection.cursor() as cursor:
+            #     cursor.execute(
+            #         """DROP TABLE users;"""
+            #     )
+                
+            #     print("[INFO] Table was deleted")
         
-    #     # the cursor for perfoming database operations
-    #     # cursor = connection.cursor()
-        
-    #     with connection.cursor() as cursor:
-    #         cursor.execute(
-    #             "SELECT version();"
-    #         )
-            
-    #         print(f"Server version: {cursor.fetchone()}")
-            
-    #     # create a new table
-    #     # with connection.cursor() as cursor:
-    #     #     cursor.execute(
-    #     #         """CREATE TABLE users(
-    #     #             id serial PRIMARY KEY,
-    #     #             first_name varchar(50) NOT NULL,
-    #     #             nick_name varchar(50) NOT NULL);"""
-    #     #     )
-            
-    #     #     # connection.commit()
-    #     #     print("[INFO] Table created successfully")
-            
-    #     # insert data into a table
-    #     # with connection.cursor() as cursor:
-    #     #     cursor.execute(
-    #     #         """INSERT INTO users (first_name, nick_name) VALUES
-    #     #         ('Oleg', 'barracuda');"""
-    #     #     )
-            
-    #     #     print("[INFO] Data was succefully inserted")
-            
-    #     # get data from a table
-    #     # with connection.cursor() as cursor:
-    #     #     cursor.execute(
-    #     #         """SELECT nick_name FROM users WHERE first_name = 'Oleg';"""
-    #     #     )
-            
-    #     #     print(cursor.fetchone())
-            
-    #     # delete a table
-    #     # with connection.cursor() as cursor:
-    #     #     cursor.execute(
-    #     #         """DROP TABLE users;"""
-    #     #     )
-            
-    #     #     print("[INFO] Table was deleted")
-    
-    # except Exception as _ex:
-    #     print("[INFO] Error while working with PostgreSQL", _ex)
-    # finally:
-    #     if connection:
-    #         # cursor.close()
-    #         connection.close()
-    #         print("[INFO] PostgreSQL connection closed")
+        except Exception as _ex:
+            print("[INFO] Error while working with PostgreSQL", _ex)
+        finally:
+            if connection:
+                # cursor.close()
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
 
-# bot = BotText()
-# print(bot.text_welcome())
+Botrecord().db_language()
 
 # Здесь будет парсер
